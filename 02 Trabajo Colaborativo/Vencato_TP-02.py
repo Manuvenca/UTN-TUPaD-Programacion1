1e3812385c39320fa449c0a0c1a010f76ea39dcf
Manuvenca <franco.vencato@tupad.utn.edu.ar>
2025-08-24 17:32:23 -0300

    renombre y organizacion

---
+++ 02 Trabajo Colaborativo/Vencato_TP-02.py
@@ -0,0 +1,10 @@
+# UTN-TUPaD-Programacion1
+# UTN-TUPaD-Programacion1
+💻 Programación 1
+Tecnicatura Universitaria en Programación
+📍 Universidad Tecnológica Nacional
+
+✨ Estudiante
+Nombre: Vencato Franco
+Comisión: Ag25-1C-12, R- Mendoza
+En este apartado vimos la configuacion e implementacion del GitHub Desktop
